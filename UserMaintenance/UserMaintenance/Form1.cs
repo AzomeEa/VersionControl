76562283bed4e6552ec68a936232b7f04961a0e9
AzomeEa <eniko.agotai@bitclub.hu>
2021-10-01 00:35:18 +0200

    6.

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
@@ -18,6 +19,21 @@ namespace UserMaintenance
             lblLastName.Text = Resource1.LastName; // label1
             lblFirstName.Text = Resource1.FirstName; // label2
             btnAdd.Text = Resource1.Add; // button1
+        }
+
+        BindingList<User> users = new BindingList<User>();
+        // listbox1
+        listUsers.DataSource = users;
+        listUsers.ValueMember = "ID";
+        listUsers.DisplayMember = "FullName";
+private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }

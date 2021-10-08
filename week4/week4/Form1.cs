917bff87dee68ce92f89452a70acf4c68ad81c62
AzomeEa <eniko.agotai@bitclub.hu>
2021-10-08 16:43:07 +0200

    6. feladat

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -14,6 +14,10 @@ namespace week4
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         RealEstateEntities1 context = new RealEstateEntities1();
         List<Flat> Flats;
         public Form1()
@@ -25,6 +29,31 @@ namespace week4
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         /*private void Form1_Load(object sender, EventArgs e)

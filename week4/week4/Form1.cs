208ce57f99960b1e923fb73a7b2814f1f3a49e2b
AzomeEa <eniko.agotai@bitclub.hu>
2021-10-08 16:31:59 +0200

    3. és 4. feladatok

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -12,14 +12,21 @@ namespace week4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities1 context = new RealEstateEntities1();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void LoadData()
         {
-
+            Flats = context.Flats.ToList();
         }
+
+        /*private void Form1_Load(object sender, EventArgs e)
+        {
+        }*/
     }
 }

4453cf2a16fed04e67112f8247441cee80be1644
AzomeEa <eniko.agotai@bitclub.hu>
2021-11-25 11:42:58 +0100

    Jelszó validálás teszt

--- week9/UnitTestExample/AccountControllerTestFixture.cs
+++ week9/UnitTestExample/AccountControllerTestFixture.cs
@@ -26,5 +26,14 @@ namespace UnitTestExample
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+            Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Ab1234", false),
+            TestCase("Abcd1234", true)
+        ]
     }
 }
